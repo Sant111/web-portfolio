9286d88993364b1626df8c37340cf36d4f31e39d
Sant <Sant@Sants-MacBook-Pro.local>
2020-12-06 12:15:31 +0000

    Initial commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+import Home from './pages/Home'
+import Cv from './pages/Cv'
+import Projects from './pages/Projects'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/'exact component={Home}/>
+          <Route path='/cv' component={Cv}/>
+          <Route path='/projects' component={Projects}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Cv.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Cv() {
+    return (
+        <div>
+            <div className='cv'>
+                <h1>CV</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Cv
